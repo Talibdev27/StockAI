e4aa4f66e8918bb781e2f32a440e26daeda0dfec
Talibdev27 <mukhammadaminkhonesaev@gmail.com>
2025-11-11 17:11:17 +0000

    Remove unnecessary files
    - Delete attached_assets directory (screenshots, pasted text)
    - Remove example components directory
    - Delete temporary Jupyter notebooks (Untitled*.ipynb)
    - Remove test_improvements.py
    - Delete poster_prompt.md and design_guidelines.md
    - Remove duplicate python_backend/python_backend directory
    - Update .gitignore to exclude temporary files

--- client/src/components/examples/RiskDisclaimer.tsx
+++
@@ -1,9 +0,0 @@
-import RiskDisclaimer from "../RiskDisclaimer";
-
-export default function RiskDisclaimerExample() {
-  return (
-    <div className="p-6">
-      <RiskDisclaimer />
-    </div>
-  );
-}
